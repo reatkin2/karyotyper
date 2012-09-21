4004d5c83dfda549f406a0a44c06951355f4824b
Alex Kesling <ahkeslin@ncsu.edu>
2012-09-21 11:14:06 -0400

    Adding Apache Commons Math

---
+++ project/KaryoTyper/src/MedialAxis/RegressedLine.java
@@ -0,0 +1,16 @@
+/**
+ * 
+ */
+package MedialAxis;
+
+import org.apache.commons.math3.optimization.
+
+/**
+ * @author ahkeslin
+ *
+ */
+public class RegressedLine {
+	public RegressedLine() {
+		PolynomialFitter
+	}
+}
