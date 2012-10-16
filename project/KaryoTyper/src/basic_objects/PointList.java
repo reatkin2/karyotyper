6cf0a6cae48fcc9b4f9ff009e894b161ebc9d9db
Andrew McKnight <mcknightaa@gmail.com>
2012-10-15 23:00:48 -0400

    trying to merge to aamcknig from oldstate

---
+++ project/KaryoTyper/src/basic_objects/PointList.java
@@ -0,0 +1,36 @@
+package basic_objects;
+
+import java.awt.Point;
+import java.util.LinkedList;
+
+public class PointList {
+	private LinkedList<Point> pointList;
+	private int distanceFromEdge;
+	public PointList(){
+		pointList=null;
+		distanceFromEdge=-1;
+	}
+	public PointList(Point tempPoint,int distance){
+		distanceFromEdge=distance;
+		pointList=new LinkedList<Point>();
+		pointList.add(tempPoint);
+	}
+	public void setList(PointList tempList){
+		pointList=(LinkedList<Point>)tempList.getList().clone();
+		distanceFromEdge=tempList.getDistanceFromEdge();
+	}
+	public boolean addPoint(Point tempPoint, int distance){
+		if(distance==distanceFromEdge){
+			pointList.add(tempPoint);
+			return true;
+		}
+		return false;
+	}
+	public LinkedList<Point> getList(){
+		return pointList;
+	}
+	public int getDistanceFromEdge() {
+		return distanceFromEdge;
+	}
+
+}
